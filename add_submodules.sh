8c18b8cdf716415f2dff6296f6fef9cd76b0760a
K Lauer <klauer@slac.stanford.edu>
2021-01-26 10:53:55 -0800

    Missing repos

--- add_submodules.sh
+++ add_submodules.sh
@@ -1,16 +1,29 @@
-git submodule add git@github.com:pcdshub/lcls-plc-crixs-motion
-git submodule add git@github.com:pcdshub/lcls-plc-crixs-vac
-git submodule add git@github.com:pcdshub/lcls-plc-kfe-gmd-vac
-git submodule add git@github.com:pcdshub/lcls-plc-kfe-motion
-git submodule add git@github.com:pcdshub/lcls-plc-kfe-rix-motion
-git submodule add git@github.com:pcdshub/lcls-plc-kfe-rix-vac
-git submodule add git@github.com:pcdshub/lcls-plc-kfe-vac
-git submodule add git@github.com:pcdshub/lcls-plc-kfe-xgmd-vac
-git submodule add git@github.com:pcdshub/lcls-plc-lamp-vac
-git submodule add git@github.com:pcdshub/lcls-plc-lfe-motion
-git submodule add git@github.com:pcdshub/lcls-plc-lfe-vac
-git submodule add git@github.com:pcdshub/lcls-plc-tmo-motion
-git submodule add git@github.com:pcdshub/lcls-plc-tmo-vac
-git submodule add git@github.com:pcdshub/lcls-plc-vls-vac
-git submodule add git@github.com:pcdshub/lcls-plc-vonhamos-motion
-git submodule add git@github.com:pcdshub/lcls-twincat-motion
+add_submodule() {
+    SUBMOD=$1
+    if [ ! -d "$SUBMOD" ]; then
+        git submodule add git@github.com:pcdshub/$SUBMOD
+    fi
+}
+
+add_submodule lcls-plc-crixs-motion
+add_submodule lcls-plc-crixs-vac
+add_submodule lcls-plc-kfe-gmd-vac
+add_submodule lcls-plc-kfe-motion
+add_submodule lcls-plc-kfe-rix-motion
+add_submodule lcls-plc-kfe-rix-motion
+add_submodule lcls-plc-kfe-rix-vac
+add_submodule lcls-plc-kfe-rix-vac
+add_submodule lcls-plc-kfe-vac
+add_submodule lcls-plc-kfe-xgmd-vac
+add_submodule lcls-plc-lamp-vac
+add_submodule lcls-plc-lfe-motion
+add_submodule lcls-plc-lfe-vac
+add_submodule lcls-plc-rixs-optics
+add_submodule lcls-plc-tmo-motion
+add_submodule lcls-plc-tmo-vac
+add_submodule lcls-plc-vls-vac
+add_submodule lcls-plc-vonhamos-motion
+add_submodule lcls-plc-mfx-be-lens-interlock
+
+# Supporting modules:
+# add_submodule lcls-twincat-motion
