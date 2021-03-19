9da6e5dc612ad44752717348af12c697be5c7838
Ken Lauer <klauer@slac.stanford.edu>
2021-03-19 15:48:12 -0700

    MNT: new submodules

--- add_submodules.sh
+++ add_submodules.sh
@@ -5,17 +5,20 @@ add_submodule() {
     fi
 }
 
-
 add_submodule lcls-plc-crixs-motion
 add_submodule lcls-plc-crixs-vac
+add_submodule lcls-plc-cvmi-motion
 add_submodule lcls-plc-cvmi-vac
+add_submodule lcls-plc-hxx-vonhamos
 add_submodule lcls-plc-kfe-arbiter
+add_submodule lcls-plc-kfe-gatt
 add_submodule lcls-plc-kfe-gmd-vac
 add_submodule lcls-plc-kfe-motion
 add_submodule lcls-plc-kfe-rix-motion
 add_submodule lcls-plc-kfe-rix-vac
 add_submodule lcls-plc-kfe-vac
 add_submodule lcls-plc-kfe-xgmd-vac
+add_submodule lcls-plc-lamp-motion
 add_submodule lcls-plc-lamp-vac
 add_submodule lcls-plc-las-bts
 add_submodule lcls-plc-lfe-arbiter
@@ -26,11 +29,13 @@ add_submodule lcls-plc-lfe-optics
 add_submodule lcls-plc-lfe-vac
 add_submodule lcls-plc-liquid-jet
 add_submodule lcls-plc-mfx-be-lens-interlock
+add_submodule lcls-plc-mrco-motion
 add_submodule lcls-plc-rixs-optics
 add_submodule lcls-plc-roving-spectrometer
 add_submodule lcls-plc-tmo-motion
 add_submodule lcls-plc-tmo-optics
 add_submodule lcls-plc-tmo-vac
+add_submodule lcls-plc-txi-optics
 add_submodule lcls-plc-vls-vac
 add_submodule lcls-plc-vonhamos-motion
 add_submodule sample-delivery-system
