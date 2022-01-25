3f13bc38b9858dc08eebd8ba206f84b0dd81c122
Ken Lauer <klauer@slac.stanford.edu>
2022-01-25 09:51:28 -0800

    MNT: update submodule listing

--- add_submodules.sh
+++ add_submodules.sh
@@ -18,6 +18,8 @@ add_submodule lcls-plc-kfe-rix-motion
 add_submodule lcls-plc-kfe-rix-vac
 add_submodule lcls-plc-kfe-vac
 add_submodule lcls-plc-kfe-xgmd-vac
+add_submodule lcls-plc-lamp-lv17-motion
+add_submodule lcls-plc-lamp-lv17-vac
 add_submodule lcls-plc-lamp-motion
 add_submodule lcls-plc-lamp-vac
 add_submodule lcls-plc-las-bts
@@ -29,15 +31,25 @@ add_submodule lcls-plc-lfe-optics
 add_submodule lcls-plc-lfe-vac
 add_submodule lcls-plc-liquid-jet
 add_submodule lcls-plc-mfx-be-lens-interlock
+add_submodule lcls-plc-mfx-motion
 add_submodule lcls-plc-mrco-motion
+add_submodule lcls-plc-mrco-vac
+add_submodule lcls-plc-qrix-motion
+add_submodule lcls-plc-qrix-vac
+add_submodule lcls-plc-rix-sds-injector
 add_submodule lcls-plc-rixs-optics
 add_submodule lcls-plc-roving-spectrometer
 add_submodule lcls-plc-tmo-motion
 add_submodule lcls-plc-tmo-optics
+add_submodule lcls-plc-tmo-spectrometer-motion
 add_submodule lcls-plc-tmo-vac
 add_submodule lcls-plc-txi-optics
 add_submodule lcls-plc-vls-vac
 add_submodule lcls-plc-vonhamos-motion
+add_submodule lcls-plc-xcs-liquid-jet
+add_submodule lcls-plc-xcs-per-vac
+add_submodule lcls-plc-xpp-jjslit-motion
+add_submodule lcls-plc-xrt-optics
 add_submodule sample-delivery-system
 
 # Test/source projects:
