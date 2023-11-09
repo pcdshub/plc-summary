c8b9ed3b73fa5790ad05faaf472839be69b81d98
Ken Lauer <klauer@slac.stanford.edu>
2023-11-09 14:43:51 -0800

    FIX: adjust width for wide monitors

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ html_theme = "sphinx_rtd_theme"
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-html_css_files = ["tree.css"]
+html_css_files = ["tree.css", "width.css"]
 
 source_suffix = {
     '.rst': 'restructuredtext',
